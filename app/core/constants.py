a3dc5320b00baeacef88cfb5257b1d2af6f74561
BOOK-4S3T9GFQB9\2jooyoung <2jooyoung1123@gmail.com>
2026-03-27 22:59:18 +0900

    feat: 크롤링 사전 설계 (상수 정의, 크롤러 설정)
    - app/core/constants.py: 장르, 세부 카테고리 목록 정의 (크롤러+백엔드 공용)
    - crawler/config.py: 교보문고 카테고리 코드 매핑, URL, 크롤링 딜레이 설정
    - MVP 세부 카테고리 10개 확정

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,6 +1,9 @@
 # 팀원(크롤러)이 분류해 줄 대분류(Genre)와 세부 카테고리(Sub-category) 매핑표
 IT_BOOK_CATEGORIES = {
-    "IT": ["Spring", "FastAPI", "Node.js", "Django", "JPA", "Database"],
+    "IT": [
+        "컴퓨터공학", "IT일반", "OS", "네트워크", "보안/해킹",
+        "데이터베이스", "개발방법론", "웹프로그래밍", "프로그래밍 언어", "모바일프로그래밍",
+    ],
 }
 
 # 대분류(Genre) 리스트
